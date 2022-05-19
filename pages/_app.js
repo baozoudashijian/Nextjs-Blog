0ed6c23572b652a985442608232506464bf0dd4d
Roger Zhang <1787009521@qq.com>
2022-05-20 00:04:10 +0800

    feat: 不同css引用方法存在规则

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import '../styles/global.css'
+import '../styles/normal.css'
 
 export default function App({ Component, pageProps }) {
 
--- pages/article/index.js
+++ pages/article/index.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
+import styles from "../../styles/Article.module.css"
 
-console.log('同构代码运行在两个环境 1')
+
 export default function Article() {
-    console.log('同构代码运行在两个环境 2')
+
     return (
         <>
-            <div>文章</div>
-            {console.log('同构代码运行在两个环境 3')}
+            <div className={`article bg ${styles.size}`}>文章</div>
             <Link href="/">返回首页</Link>
+            <style jsx>
+                {
+                    `
+                        .article {
+                            color: #c00
+                        }
+                    `
+                }
+            </style>
         </>
     )
 }
