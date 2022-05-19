38ebec0f3bc93942f74ffbc5d637b9c7eae62aa0
Roger Zhang <1787009521@qq.com>
2022-05-19 23:32:05 +0800

    feat: 引入全局样式

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
+
     return (
         <div className="zrj">
             <Head>
