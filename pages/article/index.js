0056e6bf81b3ffdd684e09145f55a95559e2e549
Roger Zhang <1787009521@qq.com>
2022-05-21 10:32:35 +0800

    feat: 引入local image和remote image

--- pages/article/index.js
+++ pages/article/index.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
-import styles from "../../styles/Article.module.css"
+import styles from "styles/Article.module.css"
+import png from 'public/109951166463602925.jpeg'
+import Image from 'next/image'
 
-
+console.log(png, 'png')
 export default function Article(props) {
 
     console.log(props, 'article')
@@ -9,6 +11,15 @@ export default function Article(props) {
     return (
         <>
             <div className={`article bg ${styles.size}`}>文章</div>
+            <div>
+                <Image src={png} />
+                <Image
+                    src="/109951167165747391.jpeg"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                />
+            </div>
             <Link href="/">返回首页</Link>
             <style jsx>
                 {
