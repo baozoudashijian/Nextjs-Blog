5a00790ceb846458de635e85c8d17898b206cc3e
Roger Zhang <1787009521@qq.com>
2022-05-21 13:38:44 +0800

    feat: 不推荐使用ne xnext-images插件

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// const withImages = require('next-images')
+
+// module.exports = withImages({
+//   webpack(config, options) {
+//     console.log(config, 'config')
+//     return config
+//   },
+//   images: {
+//     disableStaticImages: true
+//   }
+// })
--- pages/article/index.js
+++ pages/article/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import styles from "styles/Article.module.css"
 import png from 'public/109951166463602925.jpeg'
+import png2 from "assets/images/109951167135420611.jpeg"
 import Image from 'next/image'
 
 console.log(png, 'png')
+console.log(png2, 'png')
 export default function Article(props) {
 
     console.log(props, 'article')
@@ -19,6 +21,7 @@ export default function Article(props) {
                     width={500}
                     height={500}
                 />
+                <img src={png2.src} />
             </div>
             <Link href="/">返回首页</Link>
             <style jsx>
